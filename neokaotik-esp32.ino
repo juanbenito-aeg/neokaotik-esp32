8a876f94282cf6c3d8bc0e957252a4c0ea1d5e84
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2025-10-21 18:44:50 +0200

    feat(NK-111): set up the ultrasonic sensor & update the distance between it & an object/person every second

---
+++ neokaotik-esp32.ino
@@ -0,0 +1,47 @@
+// Define the pins for the ultrasonic sensor
+const int trigPin = 12;
+const int echoPin = 14;
+
+// Define the sound speed in cm/uS
+#define SOUND_SPEED 0.034
+
+long duration; // Saves the travel time of the ultrasonic waves (time elapsed since transmission and reception of the pulse wave)
+float distance; // Saves the distance to an object/person in centimeters
+
+void setup() {
+  Serial.begin(115200); // Starts the serial communication
+
+  setUpUltrasonicSensor();
+}
+
+void setUpUltrasonicSensor() {
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+}
+
+void loop() {
+  updateDistanceToObjectOrPerson();
+}
+
+void updateDistanceToObjectOrPerson() {
+  // Clears the trigPin (gives a short LOW pulse to ensure a clean HIGH pulse will be gotten)
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+
+  // Calculate the distance
+  distance = duration * SOUND_SPEED/2;
+
+  // Prints the distance in the Serial Monitor
+  Serial.print("Distance in cm: " );
+  Serial.println(distance);
+
+  delay(1000);
+}
